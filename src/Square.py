b28d9a497ecde4e9be904cb5b5a05bd3123c989e
Tyler Zeller <tylermzeller@gmail.com>
2016-11-27 21:22:54 -0500

    Add digital dump of board image for matlab processing

--- src/Square.py
+++ src/Square.py
@@ -26,26 +26,26 @@ class Square:
 			_vertext_corners: list of image coordinates of this square's coordinates in
 								clockwise order
 		"""
-		#=====[ Step 1: algebraic notation/colors	]=====
+		# Step 1: algebraic notation/colors
 		self.an = algebraic_notation
 		self.get_colors (self.an)
 		self.get_vertices (BIH)
 
-		#=====[ Step 2: init image regions	]=====
+		# Step 2: init image regions
 		self.image_region = None
 		self.last_image_region = None
 
-		#=====[ Step 3: init image regions normalized	]=====
+		# Step 3: init image regions normalized
 		self.image_region_normalized = None
 		self.last_image_region_normalized = None
 
-		#=====[ Step 4: init contents histograms ]=====
+		# Step 4: init contents histograms
 		self.color_hist = None
 		self.color_hist_prev = None
 		self.contents_histogram = None
 		self.last_contents_histogram = None
 
-		#=====[ Step 5: init edges	]=====
+		# Step 5: init edges
 		self.edges = None
 		self.last_edges = None
 
@@ -57,17 +57,12 @@ class Square:
 			-----------------
 			prints out a string representation of the square
 		"""
-		title = "=====[ 	Square: " + str(self.index) + " ]====="
+		title = " 	Square: " + str(self.index) + " "
 		top_left = "top left: " + str(self.vertex_coords[0])
 		top_right = "top right: " + str(self.vertex_coords[1])
 		bottom_right = "bottom right " + str(self.vertex_coords[2])
 		bottom_left = "bottom left: " + str(self.vertex_coords[3])
 		return '\n'.join ([title, top_left, top_right, bottom_right, bottom_left])
-
-
-	####################################################################################################
-	##############################[ --- FIND SQUARE PARAMETERS --- ]####################################
-	####################################################################################################
 
 	def get_colors (self, algebraic_notation):
 		"""
@@ -94,15 +89,6 @@ class Square:
 		self.board_vertices	= algebraic_notation_to_board_coords (self.an)
 		self.image_vertices	= [board_to_image_coords (BIH, bv) for bv in self.board_vertices]
 
-
-
-
-
-
-	####################################################################################################
-	##############################[ --- GETTING IMAGE REGION --- ]######################################
-	####################################################################################################
-
 	def update_image_region (self, image):
 		"""
 			PRIVATE: update_image_region
@@ -113,24 +99,24 @@ class Square:
 		"""
 		self.last_image_region = self.image_region
 
-		#=====[ Step 1: get bounding box coordinates	]=====
+		# Step 1: get bounding box coordinates
 		iv = np.array (self.image_vertices).astype(int)
 		x_min, x_max = min(iv[:, 0]), max(iv[:, 0])
 		y_min, y_max = min(iv[:, 1]), max(iv[:, 1])
 
-		#=====[ Step 2: extract image region 	]=====
+		# Step 2: extract image region
 		self.image_region = deepcopy(image[y_min:(y_max+1), x_min:(x_max + 1)])
 
-		#=====[ Step 3: create mask	]=====
+		# Step 3: create mask
 		self.image_region_mask = np.zeros (self.image_region.shape[:2], dtype=np.uint8)
 		mask_coords = [(i[0] - x_min, i[1] - y_min) for i in self.image_vertices]
 		cv2.fillConvexPoly (self.image_region_mask, np.array(mask_coords).astype(int), 1)
 
-		#=====[ Step 4: apply mask (turns outside pixels to black)	]=====
+		# Step 4: apply mask (turns outside pixels to black)
 		idx = (self.image_region_mask == 0)
 		self.image_region[idx] = 0
 
-		#=====[ Step 5: get intensity normalized image region	]=====
+		# Step 5: get intensity normalized image region
 		self.last_image_region_normalized = self.image_region_normalized
 		totals = np.sum (self.image_region, 2, dtype=np.float) + 1
 		self.image_region_normalized = self.image_region.astype(np.float)
@@ -139,15 +125,7 @@ class Square:
 		self.image_region_normalized[:, :, 0] = np.divide (self.image_region_normalized[:, :, 0], totals) * 255
 		self.image_region_normalized[:, :, 1] = np.divide (self.image_region_normalized[:, :, 1], totals) * 255
 		self.image_region_normalized[:, :, 2] = np.divide (self.image_region_normalized[:, :, 2], totals) * 255
-		self.image_region_normalized = self.image_region_normalized.astype (np.uint8);
-
-
-
-
-
-	####################################################################################################
-	##############################[ --- WORKING WITH COLOR HIST --- ]###################################
-	####################################################################################################
+		self.image_region_normalized = self.image_region_normalized.astype (np.uint8)
 
 	def get_image_region_pixels (self, image_region):
 		"""
@@ -166,13 +144,13 @@ class Square:
 			-------------------------
 			given a kmeans predictor, this finds self.color_hist
 		"""
-		#=====[ Step 1: get list of pixels in image region	]=====
+		# Step 1: get list of pixels in image region
 		pixels = self.get_image_region_pixels (self.image_region)
 
-		#=====[ Step 2: get labels for each pixel	]=====
+		# Step 2: get labels for each pixel
 		labels = kmeans.predict (pixels)
 
-		#=====[ Step 3: get a histogram out output values	]=====
+		# Step 3: get a histogram out output values
 		self.color_hist_prev = self.color_hist
 		self.color_hist = Counter (labels)
 
@@ -183,21 +161,14 @@ class Square:
 			-----------------------------
 			given a color index, returns the change over the past two frames
 		"""
-		#=====[ Step 1: get color values	]=====
+		# Step 1: get color values
 		cur, prev = self.color_hist[color_index], self.color_hist_prev[color_index]
 
-		#=====[ Step 2: get normalization constants	]=====
+		# Step 2: get normalization constants
 		cur = float(cur) / float(sum(self.color_hist.values ()))
 		prev = float(prev) / float(sum(self.color_hist_prev.values ()))
 
 		return cur - prev
-
-
-
-
-	####################################################################################################
-	##############################[ --- EXTRACTING INFO FROM IMAGE --- ]################################
-	####################################################################################################
 
 	def piece_added_or_subtracted (self):
 		"""
@@ -207,11 +178,11 @@ class Square:
 			'subtracted' if it seems that a piece has been
 			subtracted
 		"""
-		#=====[ Step 1: get diffs from initial hist ]=====
+		# Step 1: get diffs from initial hist
 		current_diff = cv2.compareHist (self.contents_histogram, self.unoccluded_histogram, 3)
 		last_diff = cv2.compareHist (self.last_contents_histogram, self.unoccluded_histogram, 3)
 
-		#=====[ Step 2: return whichever is greater	]=====
+		# Step 2: return whichever is greater
 		if current_diff > last_diff:
 			return 'entered'
 		else:
@@ -236,20 +207,13 @@ class Square:
 			sets self.occlusion in response to the current contents
 			of the square
 		"""
-		#=====[ Step 1: get occlusion change features	]=====
+		# Step 1: get occlusion change features
 		self.occlusion_change = self.get_occlusion_change_features ()
 
-		#=====[ Step 2: get occlusion change *direction*	]=====
+		# Step 2: get occlusion change *direction*
 		self.occlusion_change_direction = self.piece_added_or_subtracted ()
 
 		return self.occlusion_change, self.occlusion_change_direction
-
-
-
-
-	####################################################################################################
-	##############################[ --- DRAWING --- ]###################################################
-	####################################################################################################
 
 	def draw_surface (self, image, color=None):
 		"""
@@ -259,11 +223,20 @@ class Square:
 		"""
 		if color == None:
 			color = self.draw_color
-		# cv2.fillConvexPoly(image, np.array(self.image_vertices).astype(int), self.draw_color) #filled
 		iv = np.array(self.image_vertices).astype(int)
 		for i in range(len(self.image_vertices)):
 			cv2.line (image, tuple(iv[i % len(self.image_vertices)]), tuple(iv[(i+1) % len(self.image_vertices)]), color, thickness=2)
 		return image
+
+	def dump(self, image, digit):
+		"""
+			PUBLIC: dump
+			--------------------
+			dumps the digit on the image where this square's region lies
+		"""
+		cv2.fillConvexPoly(image, np.array(self.image_vertices).astype(int), digit)
+		return image
+
 
 
 	def draw_vertices (self, image):

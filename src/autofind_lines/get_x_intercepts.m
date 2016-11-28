db9c2a41ac03061c0973674c134828626b42cf8a
Tyler Zeller <tylermzeller@gmail.com>
2016-11-27 19:00:19 -0500

    Add board detection

---
+++ src/autofind_lines/get_x_intercepts.m
@@ -0,0 +1,26 @@
+function x_intercepts = get_x_intercepts (lines_rt, image_width)
+% Function: get_y_intercepts 
+% --------------------------
+% given a set of lines in (rho, theta) for mat, where 
+% the ith column is the ith line and top row is rho, 
+% this returns their y intercept on the image 
+
+	%=====[ Step 1: convert lines to homogenous	]=====	
+	rhos 				= lines_rt(1, :);
+	thetas 				= lines_rt(2, :);
+	lines_homogenous	= [cos(thetas); sin(thetas); -rhos];
+
+	%=====[ Step 2: get homogenous coordinates for line on bottom	]=====
+	line_baseline = [1; 0; 0];
+	line_baseline = repmat(line_baseline, 1, size(lines_homogenous, 2));
+
+	intersections_points = cross (lines_homogenous, line_baseline, 1);
+
+	x_intercepts = intersections_points (2, :) ./ intersections_points (3, :);
+    
+    
+
+
+
+
+

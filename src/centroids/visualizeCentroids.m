b8d25f5d6c6e99d371cf329bc7117114e8d4964b
David Soller <soller.23@osu.edu>
2016-11-30 09:32:20 -0500

    Completed Uncleaned Centroid Visualization

---
+++ src/centroids/visualizeCentroids.m
@@ -0,0 +1,12 @@
+board = double(imread('startingBoard.jpg'));
+
+% Assuming r, c
+centroids = csvread('centroids.csv');
+
+hold on;
+imagesc(board/255);
+axis('image');
+axis ij;
+plot(centroids(:, 2), centroids(:, 1), '+', 'Color', 'b');
+hold off;
+pause;

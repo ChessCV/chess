0fb3a076fbf09188a95fc15bf5d916ed65d5ab76
Tyler Zeller <tylermzeller@gmail.com>
2016-11-30 22:32:06 -0500

    Add ability to search for theta buckets in finding vertical hough lines.

--- src/test.py
+++ src/test.py
@@ -1,11 +1,6 @@
-import sys
-from copy import deepcopy
-import os
 import cv2
 import numpy as np
-import sklearn
 import pickle
-import CVAnalysis
 from Board import Board
 from util import *
 
@@ -14,10 +9,12 @@ def preprocess_frame (frame):
 
 def main():
 	corner_classifier = pickle.load(open('corner_data/corner_classifier.clf', 'r'))
-	board = Board(corner_classifier=corner_classifier)
+	board = Board(corner_classifier=corner_classifier, search_for_theta=True)
 
 	#empty_filename = '../data/toy_images/empty_board.jpg'
-	empty_filename = '../data/toy_images/ChessBoard.png'
+	empty_filename = '../data/toy_images/checker.jpg'
+	#empty_filename = '../data/toy_images/ChessBoard.png'
+	#empty_filename = '../data/toy_images/chessboard4.jpg'
 
 	frame_empty_raw = cv2.imread(empty_filename)
 	frame_empty = frame_empty_raw
@@ -30,8 +27,8 @@ def main():
 	cv2.imwrite ('../data/toy_images/best_BIH.png', frame_ic)
 	cv2.imwrite ('../data/toy_images/dump.bmp', board_dump)
 
-	wait_for_image ('BIH MARKED', frame_ic)
-	wait_for_image ('BOARD DIGIT DUMP', board_dump)
+	wait_for_image ('Homography', frame_ic)
+	wait_for_image ('Board image dump', board_dump)
 
 def wait_for_image(title, image):
 	cv2.imshow (title, image)

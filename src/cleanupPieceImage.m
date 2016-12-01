63acaebf48765d5bcaa7c36a9aeabe375086c654
Joshua Kahn <j.harris.kahn@gmail.com>
2016-11-30 20:14:56 -0500

    More efficient than looping.

--- src/cleanupPieceImage.m
+++ src/cleanupPieceImage.m
@@ -1,8 +1,6 @@
 function [ outIm, centroids ] = cleanupPieceImage( im, threshold, dil )
 outIm = zeros(size(im, 1), size(im, 2));
-for i=1:dil
-    im = bwmorph(im, 'dilate');
-end
+im = bwmorph(im, 'dilate', dil);
 L = uint8(bwlabel(im, 8));
 imageProps = cell2mat(struct2cell(regionprops(L, 'Area', 'Centroid'))');
 rowId = meshgrid(1:size(imageProps, 1), 1)';
